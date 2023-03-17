c7576a04832e04659e4ac0ef2d9bccb68bd1b17b
jesusinsuadg <127786089+jesusinsuadg@users.noreply.github.com>
2023-03-17 15:54:52 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#menu a').click(function(e) {
-      e.preventDefault();
-      var target = $(this).attr('href');
-      $('html, body').animate({
-        scrollTop: $(target).offset().top
-      }, 900);
-    });
-  });
