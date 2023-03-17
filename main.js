e56c0d25153200a655b27c246eae52d4d7e3709d
jesusinsuadg <127786089+jesusinsuadg@users.noreply.github.com>
2023-03-17 15:56:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#menu a').click(function(e) {
+      e.preventDefault();
+      var target = $(this).attr('href');
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
+      }, 900);
+    });
+  });
